71fbd871ed3bcf33a25615cbbb40636a5103a45b
everysoftware <pravitel2015ify@gmail.com>
2025-04-02 17:28:09 +0300

    fix: fix security issues

--- test_client/config.py
+++ test_client/config.py
@@ -3,8 +3,8 @@ from app.base.schemas import BaseSettings
 
 class Settings(BaseSettings):
     fastid_url: str = "http://localhost:8012"
-    client_id: str = "019382ce69c278428e3fd22bc1433b9e"
-    client_secret: str = "019382ce69c278428e3fd234fa7a672d"
+    client_id: str
+    client_secret: str
 
 
 settings = Settings()
