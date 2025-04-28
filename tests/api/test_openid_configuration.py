cf65f2af58e5df8efb56e944d3f5ee81d7474eb7
everysoftware <pravitel2015ify@gmail.com>
2025-04-28 13:23:43 +0300

    feat: add test openid configuration

---
+++ tests/api/test_openid_configuration.py
@@ -0,0 +1,15 @@
+from auth365.schemas import JWKS, DiscoveryDocument
+from httpx import AsyncClient
+from starlette import status
+
+
+async def test_openid_configuration(frontend_client: AsyncClient) -> None:
+    response = await frontend_client.get("/.well-known/openid-configuration")
+    assert response.status_code == status.HTTP_200_OK
+    DiscoveryDocument.model_validate_json(response.content)
+
+
+async def test_jwks(frontend_client: AsyncClient) -> None:
+    response = await frontend_client.get("/.well-known/jwks.json")
+    assert response.status_code == status.HTTP_200_OK
+    JWKS.model_validate_json(response.content)
