569bae3ec75da7b59262e70936fb7556f2bee1f7
everysoftware <pravitel2015ify@gmail.com>
2025-05-16 19:23:20 +0300

    fix: fix "Invalid bot username" on telegram redirect page

--- fastid/oauth/router.py
+++ fastid/oauth/router.py
@@ -1,5 +1,6 @@
 from typing import Annotated, Any
 
+from aiogram import Bot
 from fastapi import APIRouter, Depends, Request, Response
 from fastapi.responses import RedirectResponse
 from fastlink.schemas import OAuth2Callback
@@ -10,6 +11,7 @@ from fastid.auth.config import auth_settings
 from fastid.auth.dependencies import UserDep, cookie_transport, get_optional_user
 from fastid.auth.models import User
 from fastid.database.schemas import PageDTO
+from fastid.notify.clients.dependencies import get_bot
 from fastid.oauth.config import oauth_settings
 from fastid.oauth.dependencies import OAuthAccountsDep, valid_callback
 from fastid.oauth.schemas import InspectProviderResponse, OAuthAccountDTO
@@ -82,17 +84,18 @@ async def oauth_revoke(
     "/redirect/telegram",
     status_code=status.HTTP_200_OK,
 )
-async def telegram_redirect(
-    request: Request,
-) -> Any:
-    return templates.TemplateResponse(
-        request,
-        "telegram-redirect.html",
-        {
-            "request": request,
-            "redirect_uri": f"{oauth_settings.base_redirect_url}/telegram",
-        },
-    )
+async def telegram_redirect(request: Request, bot: Annotated[Bot, Depends(get_bot)]) -> Any:
+    async with bot:
+        me = await bot.get_me()
+        return templates.TemplateResponse(
+            request,
+            "telegram-redirect.html",
+            {
+                "request": request,
+                "redirect_uri": f"{oauth_settings.base_redirect_url}/telegram",
+                "bot_username": me.username,
+            },
+        )
 
 
 @router.get(
