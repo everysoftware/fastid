1bb2642a44e73bcecb5d236e4ffdbbb2ed4c8e96
ivan.stasevich <pravitel2015ify@gmail.com>
2026-07-08 13:25:26 +0300

    feat: refactor FastID settings and update dependency groups in poetry.lock

--- examples/config.py
+++ examples/config.py
@@ -2,9 +2,9 @@ from fastid.core.schemas import BaseSettings
 
 
 class Settings(BaseSettings):
-    fastid_url: str = "http://localhost:8012"
-    fastid_client_id: str
-    fastid_client_secret: str
+    url: str = "http://localhost:8012"
+    client_id: str
+    client_secret: str
 
 
 settings = Settings()
--- examples/httpx.py
+++ examples/httpx.py
@@ -14,29 +14,29 @@ app = FastAPI()
 def login(request: Request) -> Any:
     params = {
         "response_type": "code",
-        "client_id": settings.fastid_client_id,
+        "client_id": settings.client_id,
         "redirect_uri": request.url_for("callback"),
         "scope": "openid",
     }
-    url = f"{settings.fastid_url}/authorize?{urlencode(params)}"
+    url = f"{settings.url}/authorize?{urlencode(params)}"
     return RedirectResponse(url=url)
 
 
 @app.get("/callback")
 def callback(code: str) -> Any:
     response = httpx.post(
-        f"{settings.fastid_url}/api/v1/token",
+        f"{settings.url}/api/v1/token",
         headers={"Content-Type": "application/x-www-form-urlencoded"},
         data={
             "grant_type": "authorization_code",
-            "client_id": settings.fastid_client_id,
-            "client_secret": settings.fastid_client_secret,
+            "client_id": settings.client_id,
+            "client_secret": settings.client_secret,
             "code": code,
         },
     )
     token = response.json()["access_token"]
     response = httpx.get(
-        f"{settings.fastid_url}/api/v1/userinfo",
+        f"{settings.url}/api/v1/userinfo",
         headers={"Authorization": f"Bearer {token}"},
     )
     return response.json()
